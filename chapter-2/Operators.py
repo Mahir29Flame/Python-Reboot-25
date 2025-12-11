9ae63e603db10c1418dfd0dd0e87a2ee817745b4
Mahir29Flame <zarif467396@gmail.com>
2025-12-11 22:46:16 +0600

    operators

--- chapter-2/Operators.py
+++ chapter-2/Operators.py
@@ -12,3 +12,47 @@ b += a
 print ("b is now",b)
 b -= 2*a
 print("It is now",b ,"!!!")
+
+# Comparison
+
+X = 8<=6
+Y = 89!=58598
+print("X's comparison is",X)
+print("Y's comparison is",Y)
+
+# Logical
+
+# Truth table of 'or'
+H = True or True
+print("True or True is",H)
+
+J = True or False
+print("True or False is",J)
+
+K = False or True
+print("False or True is",K)
+
+L = False or False
+print("False or False is",L)
+
+
+# Truth table of 'and'
+
+M = True and True
+print("True and True is",M)
+
+N = True and False
+print("True and False is",N)
+
+O = False and True
+print("False and True is",O)
+
+P = False and False
+print("False and False is",P)
+
+# Truth table of 'not'
+Q = not True
+print("not True is",Q)
+
+R = not False
+print("not False is",R)
