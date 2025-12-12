75f74effb5091b38092d3b1ecdd0ea73a8e9c9c8
Mahir29Flame <zarif467396@gmail.com>
2025-12-12 10:08:51 +0600

    types

---
+++ chapter-2/types.py
@@ -0,0 +1,11 @@
+Name = "Cow"
+Age = 2.4
+kids = 1
+Intellegent = False
+brain = None
+
+print("type of name is",type(Name))
+print("type of age is",type(Age))
+print("type of kids is",type(kids))
+print("type of Intellegent is",type(Intellegent))
+print("type of brain is",type(brain))
